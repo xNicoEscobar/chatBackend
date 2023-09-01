c666dff8c24db3b891ebad8ffaefbc2b5c33a0f1
Nicolas <xnicoescobar@gmail.com>
2023-08-31 21:17:58 -0300

    3er error

--- src/app.js
+++ src/app.js
@@ -5,15 +5,15 @@ import viewsRouter from "./router/viewsRouter.js";
 
 import {dirname} from 'path'
 import { fileURLToPath } from 'url'
-export const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const app = express();
 const httpServer = app.listen(8080, () => console.log('funcando'));
 const socketServer = new Server(httpServer)
 
 app.engine('handlebars', handlebars.engine());
-app.set('views', __dirname + '\\views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
-app.use(express.static(__dirname + '\\public'));
+app.use(express.static(__dirname + '/public'));
 
 app.use('/', viewsRouter);
