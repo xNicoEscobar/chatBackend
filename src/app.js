1b4991f6fd3cd5bf2fe8a65e57f55bb79346536c
Nicolas <xnicoescobar@gmail.com>
2023-08-31 21:04:13 -0300

    dassad

--- src/app.js
+++ src/app.js
@@ -3,13 +3,17 @@ import handlebars from "express-handlebars";
 import { Server } from "socket.io";
 import viewsRouter from "./router/viewsRouter.js";
 
+import {dirname} from 'path'
+import { fileURLToPath } from 'url'
+export const __dirname = dirname(fileURLToPath(import.meta.url))
+
 const app = express();
 const httpServer = app.listen(8080, () => console.log('funcando'));
 const socketServer = new Server(httpServer)
 
 app.engine('handlebars', handlebars.engine());
-app.set('views', './src/views');
+app.set('views', __dirname + '\\views');
 app.set('view engine', 'handlebars');
-app.use(express.static('/src/public'));
+app.use(express.static(__dirname + '\\public'));
 
 app.use('/', viewsRouter);
